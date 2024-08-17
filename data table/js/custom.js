1321c10e85bcc6a126776902d1a77c2479b8d00e
mahabub <mahabubalom72091@gmail.com>
2024-08-17 10:50:08 +0300

    adding

---
+++ data table/js/custom.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+    var table = $('#example').DataTable({
+
+        buttons:['copy', 'csv', 'excel', 'pdf', 'print', 'add', 'remove']
+    });
+
+    table.buttons().container()
+    .appendTo('#example_wrapper .col-md-6:eq(0)');
+});
+
+
+    
+
+
